839f729d10b72a49884559a6fe778e37b2c9bfa3
Caregnato Gianluca <gianluca.caregnato@gmail.com>
2025-04-16 17:26:16 +0200

    ImageDatasetGenerator completed

--- code/src/ImageDatasetGenerator.cpp
+++ code/src/ImageDatasetGenerator.cpp
@@ -1,7 +1,7 @@
 #include "../include/ImageDatasetGenerator.h"
 #include "../include/CustomErrors.h"
 
-void ImageDatasetGenerator::generateRotatedImages(const std::string& inputImage, const std::string& inputMask, const std::string& outputFolder, int numImages) {
+void ImageDatasetGenerator::generateRotatedImages(const std::string& inputImage, const std::string& inputMask, const std::string& outputFolder, int numImages, bool genLightVariations, int numLightVariations) {
     namespace fs = std::filesystem;
     double angleStep = 360.0 / numImages;
 
@@ -29,7 +29,44 @@ void ImageDatasetGenerator::generateRotatedImages(const std::string& inputImage,
         cv::warpAffine(mask, outputMask, rotMat, mask.size());
         std::string outputImmname = inputPathImage.stem().string() + "_rot_" + std::to_string(static_cast<int>(angle)) + inputPathImage.extension().string();
         cv::imwrite((fs::path(outputFolder) / outputImmname).string(), outputImm);
+
+        if(genLightVariations)
+            generateLightVariations((fs::path(outputFolder) / outputImmname).string(), outputFolder, numLightVariations);
+
         std::string outputMaskname = inputPathMask.stem().string() + "_rot_" + std::to_string(static_cast<int>(angle)) + inputPathMask.extension().string();
         cv::imwrite((fs::path(outputFolder) / outputMaskname).string(), outputMask);
+    }
+}
+
+void ImageDatasetGenerator::generateLightVariations(const std::string& inputImage, const std::string& outputFolder, int numImages) {
+    namespace fs = std::filesystem;
+
+    if (!fs::exists(outputFolder)) {
+        fs::create_directories(outputFolder);
+    }
+
+    cv::Mat imm = cv::imread(inputImage);
+    if (imm.empty()) {
+        throw CustomErrors::ImageLoadError("Error loading the image: ", inputImage);
+    }
+
+    int brightnessRange = 100; 
+    int brightnessStart = -brightnessRange / 2;
+    int brightnessStep = brightnessRange / (numImages - 1);
+
+    fs::path inputPath(inputImage);
+    int currentBrightness = brightnessStart;
+    for (int i = 0; i < numImages; i++) {
+        cv::Mat modifiedImage;
+        
+        imm.convertTo(modifiedImage, -1, 1, currentBrightness);
+
+        std::string outputName = inputPath.stem().string() + "_light_" + std::to_string(currentBrightness) + inputPath.extension().string();
+        cv::imwrite((fs::path(outputFolder) / outputName).string(), modifiedImage);
+        currentBrightness += brightnessStep;
+    }
+    namespace fs = std::filesystem;
+    if (fs::exists(inputImage)) {
+        fs::remove(inputImage);
     }
 }
--- code/src/MainGenerator.cpp
+++ code/src/MainGenerator.cpp
@@ -8,7 +8,7 @@ int main(){
     std::string Mask = "/home/gian-pc/Desktop/MiddleProject_ComputerVision/dataset/035_power_drill/models/view_0_000_mask.png";
     std::string outputFolder = "/home/gian-pc/Desktop/MiddleProject_ComputerVision/Image_generated/Power_drill_Generated";
 
-    generator.generateRotatedImages(Image,Mask ,outputFolder, 10);
+    generator.generateRotatedImages(Image,Mask ,outputFolder, 10, true, 10);
 
     //ImageDatasetGenerator::generateRotatedImages(const std::string& inputImage, const std::string& outputFolder, int numImages) 
 }
