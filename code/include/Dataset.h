0598e0c8054a1b2593d3b795b20107521b15a3f4
EndlessDoomsayer <matt.bno.tv@gmail.com>
2025-04-19 10:43:13 +0200

    fixed dataset model loading

--- code/include/Dataset.h
+++ code/include/Dataset.h
@@ -6,24 +6,82 @@
 #include <map>
 
 
+/**
+ * @brief Dataset class to manage a single object's test images, labels and models.
+ */
 class Dataset{
     private:
-    Object_Type type;   //object type of the dataset, assuming to create one dataset for every object type (sugar box, mustard, power drill)
-    std::string folderpath;     //folder where the physical files are located (to check if another dataset is the copy of this one)
-    std::vector<std::pair<std::vector<Label>, std::string>> test_items;   //vector of pairs of labels and image filepath (one image can have more labels since more than one object can appear)
-    std::vector<std::pair<std::string, std::string>> models; //pair of IMAGE COLOR - MASK
-
+    /**
+     * @brief Object_Type enum to define the type of object in the dataset.
+     */
+    Object_Type type;
+    /**
+     * @brief folderpath string to define the folder where the dataset is located.
+     * @details used to check if another dataset is a copy of this one.
+     * @note this is not used in the current implementation.
+     */ 
+    std::string folderpath;
+    /**
+     * @brief test_items vector of pairs of labels and image filepath.
+     * @details one image can have more labels since more than one object can appear, also one image can contain objects (and labels) of other objects types.
+     */
+    std::vector<std::pair<std::vector<Label>, std::string>> test_items;
+    /**
+     * @brief models vector of pairs of image and mask filepath (image first, mask second).
+     * @details the image is the colored version of the object, the mask filters out the background of the image.
+     */
+    std::vector<std::pair<std::string, std::string>> models;
+    /**
+     * @brief load model filenames (divided into images and masks filenames) from the corresponding model folder (assuming that images and masks files are mixed in the folder).
+     * @param folderpath string to define the model folder path.
+     * @param images_filenames output vector of strings storing colored images file paths.
+     * @param masks_filenames output vector of strings storing masks file paths.
+     * @return size_t number of models loaded.
+     * @details if the number of images and masks is not the same, the function will throw an error.
+     */
     const size_t load_models_filenames(const std::string& folderpath, std::vector<std::string>& images_filenames, std::vector<std::string>& masks_filenames);
 
     public:
+    /**
+     * @brief Dataset constructor, loads test items' and models' filenames from the corresponding folders. 
+     * @param type Object_Type enum to define the type of object in the dataset.
+     * @param folderpath string to define the folder where the dataset is located.
+     */
     Dataset(const Object_Type& type, const std::string& folderpath = "");
 
+    /**
+     * @brief method to load test items' filenames from the corresponding folder into class member test_items.
+     * @param folderpath string to define the test items folder path.
+     * @return size_t number of test items loaded.
+     */
     const size_t load_test_items(const std::string& folderpath);
+    /**
+     * @brief method to load models' filenames from the corresponding folder into class member models.
+     * @param folderpath string to define the models folder path.
+     * @return size_t number of models loaded.
+     * @details the function will throw an error if the number of images and masks is not the same.
+     */
     const size_t load_models(const std::string& folderpath);
 
+    /**
+     * @brief return the object type of the dataset.
+     * @return Object_Type enum to define the type of object in the dataset.
+     */
     const Object_Type& get_type() const {return this->type;}
+    /**
+     * @brief return the folder path of the dataset.
+     * @return string to define the folder where the dataset is located.
+     */
     const std::string& get_folderpath() const {return this->folderpath;}
+    /**
+     * @brief return the test items of the dataset.
+     * @return vector of pairs of labels and image filepath.
+     */
     const std::vector<std::pair<std::vector<Label>, std::string>>& get_items() const {return this->test_items;}
+    /**
+     * @brief return the models of the dataset.
+     * @return vector of pairs of image and mask filepath.
+     */
     const std::vector<std::pair<std::string, std::string>>& get_models() const {return this->models;}
 };
 
--- code/src/Dataset.cpp
+++ code/src/Dataset.cpp
@@ -4,6 +4,7 @@
 
 Dataset::Dataset(const Object_Type& type, const std::string& folderpath)
     : type{type}, folderpath{folderpath} {
+    //if the folderpath is empty, the dataset is not loaded
     if(this->folderpath == "") return;
 
     this->load_test_items(folderpath);
@@ -12,24 +13,27 @@ Dataset::Dataset(const Object_Type& type, const std::string& folderpath)
 
 const size_t Dataset::load_test_items(const std::string& folderpath){
     this->test_items.clear();
-
+    //loads the folder names in the given path (i.e. labels, models and test_images)
     std::vector<std::string> folders = Utils::Directory::get_folder_filenames(folderpath);
     
+    //iterators to store the folder paths of labels and test_images
     std::vector<std::string>::iterator label_folder = folders.end();
     std::vector<std::string>::iterator test_images_folder = folders.end();
 
     for(std::vector<std::string>::iterator it=folders.begin(); it != folders.end(); ++it){
+        //splits the folder path into directories tokens (removes the path, keeping only the last directory name)
         std::vector<std::string> tokens;
         const size_t n_t = Utils::String::split_string(*it, tokens, '/');   //splits folderpath into directories tokens
 
-        if(label_folder == folders.end()) label_folder = tokens[n_t-1].compare("labels")==0 ? it : folders.end();  //if last directory == "labels" stores current folderpath (complete) in label_folder
-        if(test_images_folder == folders.end()) test_images_folder = tokens[n_t-1].compare("test_images")==0 ? it : folders.end();  //same but change the directory name
-
+        //if the last directory is "labels" or "test_images", stores current folderpath (complete path) in label_folder or test_images_folder
+        if(label_folder == folders.end()) label_folder = tokens[n_t-1].compare("labels")==0 ? it : folders.end();
+        if(test_images_folder == folders.end()) test_images_folder = tokens[n_t-1].compare("test_images")==0 ? it : folders.end();
+        
     }
-    
+    //if the last directory is not "labels" or "test_images", the folder is not valid
     if(label_folder == folders.end() || test_images_folder == folders.end()) throw CustomErrors::MissingDirectoryError(folderpath, "MISSING DIRECTORY, DATASET IS MALFORMED");
 
-    //vectors of subdirectories
+    //loads the filenames in the label and test_images folders
     std::vector<std::string> labels_filenames = Utils::Directory::get_folder_filenames(*label_folder);
     std::vector<std::string> test_images_filenames = Utils::Directory::get_folder_filenames(*test_images_folder);
 
@@ -43,10 +47,9 @@ const size_t Dataset::load_test_items(const std::string& folderpath){
         const std::string label_file_basename = Utils::Directory::get_file_basename(*it_l);
         const std::string label_file_name_raw = Utils::Directory::remove_file_suffix(label_file_basename,'-');
 
-        std::cout << label_file_name_raw << " " << test_image_file_name_raw << std::endl;
-        //if the 2 row filenames are the same -> the pair of label-image is correct
+        //if the 2 row filenames are the same then the pair label-image is correct
         if(test_image_file_name_raw.compare(label_file_name_raw) == 0){
-            //filenames match, load label and store image filename in vector of items
+            //load label and store image filename in vector of items
             this->test_items.push_back(std::pair<std::vector<Label>, std::string>(Utils::Loader::load_label_file((*it_l)), *it_i));
         }
         else throw CustomErrors::ImageLabelMismatch((*it_i),(*it_l), "IMAGE FILENAME AND LABEL FILENAME MISMATCH");
@@ -59,48 +62,46 @@ const size_t Dataset::load_test_items(const std::string& folderpath){
 const size_t Dataset::load_models(const std::string& folderpath){
     this->models.clear();
 
+    //loads the folder names in the given path (i.e. labels, models and test_images)
     std::vector<std::string> folders = Utils::Directory::get_folder_filenames(folderpath);
     
+    //iterator to store the folder path of model folder
     std::vector<std::string>::iterator models_folder = folders.end();
 
     for(std::vector<std::string>::iterator it=folders.begin(); it != folders.end(); ++it){
+        //splits the folder path into directories tokens (removes the path, keeping only the last directory name)
         std::vector<std::string> tokens;
         const size_t n_t = Utils::String::split_string(*it, tokens, '/');   //splits folderpath into directories tokens
 
-        if(models_folder == folders.end()) models_folder = tokens[n_t-1].compare("models")==0 ? it : folders.end();  //if last directory == "labels" stores current folderpath (complete) in label_folder
-
+        //if the last directory is "models", stores current folderpath (complete path) in models_folder
+        if(models_folder == folders.end()) models_folder = tokens[n_t-1].compare("models")==0 ? it : folders.end();
     }
-    
+    //if the last directory is not "models", the folder is not valid
     if(models_folder == folders.end()) throw CustomErrors::MissingDirectoryError(folderpath, "MISSING DIRECTORY, DATASET IS MALFORMED");
 
-    //splits model folder's files into 2 vectors: one contains 3 channel images' filenames, the other contains 1 channel masks' filenames
+    //splits model folder's files into two vectors: one contains colored images' filenames, the other contains masks' filenames
     std::vector<std::string> images_filenames;
     std::vector<std::string> masks_filenames;
     Utils::Directory::split_model_img_masks(*models_folder, images_filenames, masks_filenames);
     
-    for(auto it = masks_filenames.begin(); it != masks_filenames.end(); ++it){
-        std::cout << *it << std::endl;
-    }
-    std::cout << images_filenames.size() << " " << masks_filenames.size() << std::endl;
-
+    //if the number of images and masks is not the same, the folder is not valid
     if(images_filenames.size() != masks_filenames.size()) throw CustomErrors::ImageMaskMismatch("","", "IMAGES AND MASKS ARE NOT THE SAME NUMBER");
 
+    //iterates through images and masks filenames
     std::vector<std::string>::iterator it_i = images_filenames.begin();
-    for(std::vector<std::string>::iterator it_l = masks_filenames.begin(); it_l != masks_filenames.end(); ++it_l){
-
-        std::string image_filename = it_i->substr(it_i->find_last_of("/")+1);
-        std::string image_file_basename = image_filename.substr(0, image_filename.find_first_of('.'));
-        std::string mask_filename = it_i->substr(it_i->find_last_of("/")+1);
-        std::string mask_file_basename = mask_filename.substr(0, mask_filename.find_first_of('.'));
-
-        std::cout << image_file_basename << " " << mask_file_basename << std::endl;
-
-        //if the 2 row filenames are the same -> the pair of label-image is correct
-        if(image_file_basename.compare(mask_file_basename) == 0){
-            //filenames match, load label and store image filename in vector of items
-            this->models.push_back(std::pair<std::string, std::string>(*it_i, *it_l));
+    for(std::vector<std::string>::iterator it_m = masks_filenames.begin(); it_m != masks_filenames.end(); ++it_m){
+        //raw filenames (eliminate path, keep just the file name deleting the extension and the suffix (i.e. _color or _mask))
+        std::string image_file_basename = Utils::Directory::get_file_basename(*it_i);
+        std::string image_file_name_raw = Utils::Directory::remove_file_suffix(image_file_basename,'_');
+        std::string mask_file_basename = Utils::Directory::get_file_basename(*it_m);
+        std::string mask_file_name_raw = Utils::Directory::remove_file_suffix(mask_file_basename,'_');
+        
+        //if the 2 row filenames are the same then the pair mask-image is correct
+        if(mask_file_name_raw.compare(image_file_name_raw) == 0){
+            //load mask and store image filename in vector of models
+            this->models.push_back(std::pair<std::string, std::string>(*it_i, *it_m));
         }
-        else throw CustomErrors::ImageLabelMismatch((*it_i),(*it_l), "IMAGE FILENAME AND LABEL FILENAME MISMATCH");
+        else throw CustomErrors::ImageLabelMismatch((*it_i),(*it_m), "IMAGE FILENAME AND LABEL FILENAME MISMATCH");
         ++it_i;
     }
 
